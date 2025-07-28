9fb280e2a16d44bbebc9cdc8f78c9a0e39f6543e
Arun Kumar <arun8517.beaift24@chitkara.edu.in>
2025-07-28 12:43:37 +0530

    Add files via upload

---
+++ Row_major_Order.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int arr[2][3]={{1,2,3},{4,5,6}};
+   int *base=&arr[0][0];
+   int rows=2,cols=3;
+   cout<<"ROW MAJOR ORDER ADDRESS CALCULATION: \n";
+   for(int i=0;i<=rows;i++){
+       for(int j=0;j<=cols;j++){
+           cout<<"ADDRESS OF ARR["<<i<<"]["<<j<<"]:"<<(base+i*rows+j)<<endl;
+       }
+   }
+
+    return 0;
+}
